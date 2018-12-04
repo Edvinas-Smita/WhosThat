8059f8d4f9dadc5a34deec1dbe7df0a15a2fbd9c
Teodoras Saulys <teodoras.saulys@gmail.com>
2018-12-04 11:03:47 -0800

    added token controller

---
+++ Backend/Controllers/TokenController.cs
@@ -0,0 +1,27 @@
+﻿using Backend.Logic;
+using System.Net;
+using System.Web.Http;
+
+namespace Backend.Controllers
+{
+    public class TokenController : ApiController
+    {
+        // TODO: channge to POST later
+        [AllowAnonymous]
+        public string Get(string username, string password)
+        {
+            if (CheckUser(username, password))
+            {
+                return JwtManager.GenerateToken(username);
+            }
+
+            throw new HttpResponseException(HttpStatusCode.Unauthorized);
+        }
+
+        public bool CheckUser(string username, string password)
+        {
+            // should check in the database
+            return true;
+        }
+    }
+}
